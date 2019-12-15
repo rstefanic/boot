44b80c90ef5da6524162e29021a2ead9da5c6c42
Robert Stefanic <rstefanic72@gmail.com>
2019-12-15 10:02:42 -0600

    Added keyboard interrupt; Prints key codes read to the screen

--- boot.asm
+++ boot.asm
@@ -18,33 +18,66 @@ main:
 
 	cld			; clear direction
 
-	mov ax, 0xb800		; test video memory
+	mov ax, 0xb800		; text video memory
 	mov es, ax
 
+	cli			; clear interrupts
+
+	mov bx, 0x09		; hardware interrupt number
+	shl bx, 2		; mutliply by 2 (shift left 2 bits)
+	xor ax, ax		; 0 out ax
+	mov gs, ax		; start of memory
+	mov [gs:bx], word keyhandler
+	mov [gs:bx + 2], ds	; segment
+	
 	mov si, msg
 	call clear_line
 	call sprint
 
-	mov ax, 0xb800		; video memory
-	mov gs, ax
-	mov bx, 0x0000		; 'W'=57, attrib=0F
-	mov ax, [gs:bx]
+	mov si, next
+	call clear_line
+	call sprint
+
+	sti			; set interrupt flag
+	jmp $			; loop forever
+
+keyhandler:
+	in al, 0x60		; read data from the I/O device (keyboard)
+	mov bl, al		; save it to bl
+	mov byte [port60], al
+
+	in al, 0x61		; read the keyboard control
+	mov ah, al
+	or al, 0x80		; disable bit 7
+	out 0x61, al		; write it back to the I/O device (keyboard)
+	xchg ah, al		; get the original al that we stored in ah
+	out 0x61, al		; write the original back
+
+	mov al, 0x20		; End Interrupt	by writing that we're finished
+	out 0x20, al		; 
+
+	and bl, 0x80		; key released
+	jnz keyhandle_done
 
 	call clear_line
+	mov ax, [port60]
 	mov word [reg16], ax	; look at the register
+				; reg16 is located in "printreg16.asm"
 	call printreg16
 
-halt:
-	hlt
+keyhandle_done:
+	iret			; interrupt return
 
 ; ------------------------------------------------------------------------------
 ; Constants -- db = define bytes (8 bit) in NASM
 
 msg	db 'Boot Successful.', 0
+next	db 'Press any key to echo the key code back...', 0
 
 ; -----------------------------------------------------------------------------
 ; Fill in
 
+port60 	dw 0
 times 510-($-$$) db 0 	; NASM's way of filling up 510 bytes with zeros
-db 0x55 		; The last two bytes are a boot signature
-db 0xAA			; 	mostly used from older BIOS
+dw 0xAA55		; Magic bytes at 511 and 512 to let the BIOS know
+			; 	this is bootable
